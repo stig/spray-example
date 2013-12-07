02c6ff408abc48c2867ae9e0f324f60d1fd02f37
Stig Brautaset <stig@brautaset.org>
2013-12-07 00:11:53 +0000

    Supervision strategy

--- src/main/scala/example/TopLevel.scala
+++ src/main/scala/example/TopLevel.scala
@@ -1,30 +1,44 @@
 package example
 
-import akka.actor.{ Props, ActorLogging, Actor }
+import akka.actor._
 import spray.can.Http
 import akka.io.IO
+import akka.actor.Terminated
+import akka.actor.SupervisorStrategy.{ Restart, Stop }
 
 object TopLevel {
-  def props: Props = Props(new TopLevel)
+  def props: Props = Props(new TopLevel with TopLevelConfig {
+    def interface = "localhost"
+    def port = 8080
+  })
   def name = "top-level"
 }
 
+trait TopLevelConfig {
+  def interface: String
+  def port: Int
+}
+
 class TopLevel extends Actor with ActorLogging {
+  this: TopLevelConfig =>
 
   import context._
 
   val model = actorOf(Model.props, Model.name)
+  context watch model
 
   val service = actorOf(Service.props(model), Service.name)
+  context watch service
 
-  IO(Http) ! Http.Bind(service, "localhost", 8080)
+  IO(Http) ! Http.Bind(service, interface, port)
+
+  override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+    case _ if model == sender => Stop
+    case _ if service == sender => Restart
+  }
 
   def receive = {
-    case Http.Bound =>
-      log.info("Bound!")
-
-    case Http.CommandFailed =>
-      log.error("Port busy?")
+    case Terminated(`model`) => context stop self
   }
 
 }
